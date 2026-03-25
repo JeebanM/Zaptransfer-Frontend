7c739e0f6f2a56bf3597ebdd0a21f277579c2baa
Jeeban Mohanty <jeeban123liku@gmail.com>
2026-03-26 00:47:24 +0530

    seo: move google site verification meta tag to _app.js for global coverage

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="4Klq89JtcGdB5adRxgxwLPOiW_RpU_SnXrrbyHHoRAY" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
